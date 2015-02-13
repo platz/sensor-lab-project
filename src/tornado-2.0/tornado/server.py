21c111af8eeb93a46a90d8f474690b821007e19e
Alex <atj.platz@gmail.com>
2015-02-13 17:34:45 +0100

    Python Tornado Server

---
+++ src/tornado-2.0/tornado/server.py
@@ -0,0 +1,33 @@
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+
+ 
+ 
+class WSHandler(tornado.websocket.WebSocketHandler):
+
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        print 'new connection'
+        self.write_message("Connection established")
+      
+    def on_message(self, message):
+        print 'message received: %s' % message
+        self.write_message(message)
+ 
+    def on_close(self):
+        print 'connection closed'
+ 
+ 
+application = tornado.web.Application([
+    (r'/ws', WSHandler),
+])
+ 
+ 
+if __name__ == "__main__":
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
