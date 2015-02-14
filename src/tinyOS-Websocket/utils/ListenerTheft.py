550c4dec08f70f6732e49bf5132cf307dffe51c5
Alex <atj.platz@gmail.com>
2015-02-14 16:44:15 +0100

    made the mig-python-listener websocket-ready - communication tests between tinyOS-node and websocket server successful

--- src/tinyOS-Websocket/utils/ListenerTheft.py
+++ src/tinyOS-Websocket/utils/ListenerTheft.py
@@ -1,21 +1,54 @@
 import socket
+import websocket
 import SensingTheft
 import re
 import sys
 
 rcvPort = 7777
-fwdPort = 2000
+#fwdUdpPort = 2000
+#fwdWsPort = 8888
 fwdHost = '127.0.0.1'
 
 if __name__ == '__main__':
 
     rcvSocket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
     rcvSocket.bind(('', rcvPort))
+    
+    websocket.enableTrace(True)
+    try:
+        ws = websocket.create_connection("ws://127.0.0.1:8888/wsNeu")
+    except:
+        print 'unable to connect'
 
-    # --- the next step would be forwarding via websocket
+    while True:
+        data, addr = rcvSocket.recvfrom(1024)
+        if (len(data) > 0):
+            # --- forwarding data via websocket to server
+            rpt = SensingTheft.SensingTheft(data=data, data_length=len(data))
+	
+            print addr
+            print rpt
+            #fwdSocket.send(str(rpt))
+
+            ws.send(str(rpt))
+            print("Theft: Sent")
+            result = ws.recv()
+            #print("Received {}".format(result))
+            #ws.close()
+
+
+
+
+
+
+
+    
+    '''
+    ---> this is the UDP Socket version
+
     fwdSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        fwdSocket.connect((fwdHost, fwdPort))
+        fwdSocket.connect((fwdHost, fwdUdpPort))
     except:
         print 'unable to connect'
 
@@ -28,7 +61,10 @@ if __name__ == '__main__':
             print addr
             print rpt
             fwdSocket.send(str(rpt))
-			
+    
+    <--- UDP Socket version
+    '''
+	
 			
 
 			
