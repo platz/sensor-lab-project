2c8393787dcff3cc35724e97128b8fd9b45fbaec
Alex <atj.platz@gmail.com>
2015-02-15 17:07:59 +0100

    Added URI to server and made it internet ready with DynDns

--- src/tornado-2.0/tornado/server.py
+++ src/tornado-2.0/tornado/server.py
@@ -1,48 +1,63 @@
+from websocket import WebSocketHandler
 import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
 
-class WSHandler(tornado.websocket.WebSocketHandler):
+class WSHandlerSensing(tornado.websocket.WebSocketHandler):
 
     def check_origin(self, origin):
         return True
 
     def open(self):
-        print 'new connection'
-        self.write_message('Connection established')
+        print 'new connection URI: /sensing'
+        self.write_message('Connection established URI: /sensing')
       
     def on_message(self, message):
-        print 'message received via websocket, uri: "myWs": %s' % message
-        self.write_message('Echo: ' +message)
+        print 'message received via websocket, uri: "/sensing": %s' % message
+        self.write_message('Echo Sensing: ' +message)
  
     def on_close(self):
-        print 'connection closed'
- 
-application = tornado.web.Application([
-    (r'/myWs', WSHandler),
-])
+        print 'connection closed URI: /sensing'
 
-class WSHandlerNeu(tornado.websocket.WebSocketHandler):
+class WSHandlerAlarm(tornado.websocket.WebSocketHandler):
+
     def check_origin(self, origin):
         return True
 
     def open(self):
-        print 'new connection'
-        self.write_message('Connection established')
+        print 'new connection URI: /alarm'
+        self.write_message('Connection established URI: /alarm')
 
     def on_message(self, message):
-        print 'message received via websocket, uri: "wsNeu":\n%s' % message
-        self.write_message('Echo Neuuuuuu: ' +message)
+        print 'message received via websocket, uri: "/alarm":\n%s' % message
+        self.write_message('Echo Alarm: ' +message)
 
     def on_close(self):
-        print 'connection closed'
+        print 'connection closed URI: /alarm'
+
+class WSHandlerEcho(tornado.websocket.WebSocketHandler):
+
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        print 'new connection URI: /echo'
+        self.write_message('Connection established URI: /echo')
+
+    def on_message(self, message):
+        print 'message received via websocket, uri: "/echo":\n%s' % message
+        self.write_message('Echo Echo: ' +message)
+
+    def on_close(self):
+        print 'connection closed URI: /echo'
 
 application = tornado.web.Application([
-    (r'/wsNeu', WSHandlerNeu),
+    (r'/sensing', WSHandlerSensing),
+    (r'/alarm', WSHandlerAlarm),
+    (r'/echo', WSHandlerEcho),
 ])
- 
- 
+
 if __name__ == "__main__":
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(8888)
