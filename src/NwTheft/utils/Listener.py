226b9ec71a8b84af854e84ebaba1b5ac736c91ff
Alex <atj.platz@gmail.com>
2015-02-23 17:50:09 +0100

    reorganization of project structure

--- src/NwTheft/utils/Listener.py
+++ src/NwTheft/utils/Listener.py
@@ -1,74 +1,106 @@
 import socket
 import websocket
 import Sensing
-import re
-import sys
 
 rcvPort = 7000
-#fwdUdpPort = 2000
-#fwdWsPort = 8888
-#fwdHost = '127.0.0.1'
-#alarmUri = 'alarm'
+
+def msgSensing(rpt):
+    returnMsg = ""
+	# --- splitted Message structure:
+    # delimtedRpt: [0] = seqno, [1] = sender, [2] = humidity
+    #              [3] = temperature,[4] = lightPar,[5] = lightTsr,
+    #              [6] = switchState
+
+    # --- Split the tinyOS Message
+    delimitedRpt = rpt.split(';') # split message by delimiter ';'
+
+    # --- Node ID
+    nodeId = str(delimitedRpt[1])
+
+    # --- Temperature value
+    temp2Int = float(int(delimitedRpt[3], 0))
+    temp2Int = round(-40 + 0.01 * temp2Int, 2) # convert to Celsius and round two decimals
+
+    # --- Humidity value
+    linHumid = -4.0 + 0.0405 * float(int(delimitedRpt[2], 0)) + (-2.8 * 0.000001 * float(int(delimitedRpt[2], 0)) * int(delimitedRpt[2], 0))
+    trueHumid = round((temp2Int - 25.0) * (0.01 + 0.0008 * float(int(delimitedRpt[2], 0))) + linHumid, 2)
+
+    # --- Light value
+    vPar = ((float(int(delimitedRpt[4], 0)) / 4096) * 1.5) / 100
+    vTsr = ((float(int(delimitedRpt[5], 0)) / 4096) * 1.5) / 100
+    luxPar = round(0.625 * 1000000 * vPar * 1000, 2)
+    luxTsr = round(0.769 * 100000 * vTsr * 1000, 2)
+
+    # --- Reed Switch value
+    reedSwitch = int(delimitedRpt[6], 0)
+    if(reedSwitch == 0):
+        reedMsg = "Intrusion!"
+    else:
+        reedMsg = "Safe."
+    
+    returnMsg = 'Node: ' +str(nodeId)+', Temperature: ' +str(temp2Int)+ ', linear Humid: ' +str(trueHumid)+ ', LightPar: ' +str(luxPar)+ ', LightTsr: ' +str(luxTsr)+ ', '+reedMsg
+
+    return returnMsg
+
+def msgAlarm(rpt):
+    returnMsg = ""
+	# --- splitted Message structure:
+    # delimtedRpt: [0] = seqno, [1] = sender, [2] = humidity
+    #              [3] = temperature,[4] = lightPar,[5] = lightTsr,
+    #              [6] = switchState
+
+    # --- Split the tinyOS Message
+    delimitedRpt = rpt.split(';') # split message by delimiter ';'
+
+    # --- Node ID
+    nodeId = str(delimitedRpt[1])
+
+    # --- Reed Switch value
+    reedSwitch = int(delimitedRpt[6], 0)
+    if(reedSwitch == 0):
+        reedMsg = "Intrusion!"
+    else:
+        reedMsg = "Safe."
+
+    returnMsg = 'Node: ' +str(nodeId)+ ', '+reedMsg
+
+    return returnMsg
 
 if __name__ == '__main__':
 
     rcvSocket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
     rcvSocket.bind(('', rcvPort))
-    
+
     #websocket.enableTrace(True)
     try:
-        ws = websocket.create_connection('ws://axinen.ddns.net:8888/echo')
-        #ws = websocket.create_connection('ws://%s:%s/%s' % fwdHost, fwdWsPort, alarmUri)
+        wsSensing = websocket.create_connection('ws://localhost:8888/sensing')
+        wsAlarm = websocket.create_connection('ws://localhost:8888/alarm')
+
     except:
-        print 'unable to connect'
+        print ('unable to connect')
 
     while True:
         data, addr = rcvSocket.recvfrom(1024)
         if (len(data) > 0):
             # --- forwarding data via websocket to server
             rpt = Sensing.Sensing(data=data, data_length=len(data))
-	
-            #print addr
-            print rpt
-            #fwdSocket.send(str(rpt))
+            rpt = str(rpt)
+            #print rpt
 
-            ws.send(str(rpt))
-            result = ws.recv() # for echo testing
-            print("Received: {}".format(result)) # for echo testing
+            wsSensing.send(msgSensing(rpt))
+            wsAlarm.send(msgAlarm(rpt))
 
+            resultSensing = wsSensing.recv() # for echo testing
+            resultAlarm = wsAlarm.recv()
 
-    ws.close()
-
-
-
+            # --- for echo testing
+            #print("Received: {}".format(resultSensing))
+            #print("Received: {}".format(resultAlarm))
 
 
+    wsSensing.close()
+    wsAlarm.close()
 
-
-    
-    '''
-    ---> this is the UDP Socket version
-
-    fwdSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        fwdSocket.connect((fwdHost, fwdUdpPort))
-    except:
-        print 'unable to connect'
-
-    while True:
-        data, addr = rcvSocket.recvfrom(1024)
-        #data = rcvSocket.recv(1024)
-        if (len(data) > 0):
-            rpt = SensingTheft.SensingTheft(data=data, data_length=len(data))
-			
-            print addr
-            print rpt
-            fwdSocket.send(str(rpt))
-    
-    <--- UDP Socket version
-    '''
-	
-			
 
 			
 
